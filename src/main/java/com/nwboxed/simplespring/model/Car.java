057e97a369ceed23a253416a9e595508d8e81204
Ben Settle <ben.settle@mettle.co.uk>
2023-08-04 14:49:15 +0100

    Upgrade java, spring and gradle
    Upgrade to Java17, Spring 3.1.2 and gradle 8.2.1.
    
    Fixed up build.gradle and any other changes to make it run successfully
    
    Also included RestAssured as quite a few candidates prefer that library

--- src/main/java/com/nwboxed/simplespring/model/Car.java
+++ src/main/java/com/nwboxed/simplespring/model/Car.java
@@ -1,12 +1,11 @@
-package com.eeveebank.simplespring.model;
+package com.nwboxed.simplespring.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
 
 @Data
 @Builder
